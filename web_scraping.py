3d39caa062ffeea6429de13b93ae9d0087807d4d
sandy-ji-code <56841029+sandy-ji-code@users.noreply.github.com>
2020-02-02 23:38:40 +0530

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,7 +1,11 @@
+# module for http request
 import requests
+# module for access code
 from bs4 import BeautifulSoup
 
 # paste the url
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
+
+# print code
 print(soup)
